d89ddc34df6d2af605b686a26111d49029ed6608
Rogério Brito <rbrito@gmail.com>
2021-03-01 18:19:02 -0300

    .my-scripts: Remove obsolete scripts.
    These scripts were used with ikiwiki, but they are "built-in" to jekyll (and
    other modern static site generators).

--- .my-scripts/css-minifier.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat style.css local.css | csstidy - --template=highest --optimise_shorthands=2 --remove_last_\;=true combined.min.css
--- .my-scripts/regenerate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# To be executed at the root directory of the *source* of the blog.
-ikiwiki --setup .blog.setup --verbose
-cd ../rbrito.github.com/
-git checkout -- blog/smileys/*.png blog/wikiicons/*.png blog/*.css
